0eece52d3b7e8153c6043ad5ffee9d42ef270145
GrzegorzKrug <grzesiekk321@gmail.com>
2019-06-14 23:37:28 +0200

    Supervised learning, Data collecting

--- supervised/supervised_data_grab.py
+++ supervised/supervised_data_grab.py
@@ -3,7 +3,21 @@ mypackage_path = os.path.abspath(os.getcwd() + '\..' + '\mypackage')
 sys.path.append( mypackage_path )
 from GameCards98 import GameCards98
 
+class Grab_Teaching_Data(GameCards98):
+    def __init__(self):
+        self.samples = 10000
+        self.states = []
+        pass
 
-app = GameCards98()
+    def get_game_states(self):
+        pass
 
-app.start_game()
+    def assisng_values_to_states(self):
+        pass
+
+
+
+
+app = Grab_Teaching_Data()
+
+# app.start_game()
