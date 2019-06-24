1107fa94ae7d5899ee15fc58072b07c30de36919
GrzegorzKrug <grzesiekk321@gmail.com>
2019-06-24 20:18:28 +0200

    4% win rate

--- supervised/Playing_Game_with_algoritm.py
+++ supervised/Playing_Game_with_algoritm.py
@@ -7,42 +7,47 @@ mypackage_path = os.path.abspath(os.getcwd() + '\..' + '\mypackage')
 sys.path.append(mypackage_path)
 from GameCards98 import GameCards98  # importing from diffrent directory
 
-
+win_count = 0
 my_predict = Grab_Teaching_Data.attach_score_to_state
-
-game = GameCards98()
-game.reset()
-game.hand_fill()
-score = 0
-app1 = Grab_Teaching_Data()
-while True:
+for x in range(10000):
+    game = GameCards98()
+    game.reset()
     game.hand_fill()
-    # game.display_table()
+    score = 0
+    app1 = Grab_Teaching_Data()
+    while True:
+        game.hand_fill()
+        # game.display_table()
 
-    # hand = game.hand
-    # piles = game.piles
-    # turn = game.turn
+        # hand = game.hand
+        # piles = game.piles
+        # turn = game.turn
 
 
-    # def attach_score_to_state(self, deck, hand, piles, score_min=1, best_move_only=True):
-    best_move = app1.attach_score_to_state(None, game.hand, game.piles)[0]
-    if best_move is None:
-        pass
-    else:
+        # def attach_score_to_state(self, deck, hand, piles, score_min=1, best_move_only=True):
+        best_move = app1.attach_score_to_state(None, game.hand, game.piles)[0]
+        if best_move is None:
+            pass
+        else:
 
-        hand, pile = best_move['move']
-        score_pre = game.score  # capturing score for comparison
-        game.play_card(hand, pile)
-        print('My move', hand + 1, pile + 1, '\t', 'Score gained=', game.score - score_pre)
+            hand, pile = best_move['move']
+            score_pre = game.score  # capturing score for comparison
+            game.play_card(hand, pile)
+            # print('My move', hand + 1, pile + 1, '\t', 'Score gained=', game.score - score_pre)
 
-    # input('next...')
-    status, comment = game.end_condition()
-    if status:
-        input(comment)
-    if status is not None or game.score < - 10:
+        # input('next...')
+        status, comment = game.end_condition()
+        if status:
+            # print('First win, x=', x)
+            # input(comment)
+            win_count += 1
 
-        print(comment)
-        break
+        if status is not None or game.score < - 10:
+            # print(comment)
+            break
+print('Win count =',win_count)
 
-game.display_table(show_chances=False)
+# Win count = 412
+# Win % = 4.12
+
 
