29e9bfaca99da476869ed6bba63b7d2ec1b0212b
GrzegorzKrug <50370928+GrzegorzKrug@users.noreply.github.com>
2019-09-01 22:01:08 +0200

    pep8 - files names

---
+++ src/reinforced/RL_Agent.py
@@ -0,0 +1,22 @@
+import tensorflow as tf
+import random
+from tensorflow import keras
+import numpy as np
+import math
+import matplotlib.pyplot as plt
+
+class RLAgent:
+    def __init__(self, num_states, num_actions, batch_size):
+        self._num_states = num_states
+        self._num_actions = num_actions
+        self._batch_size = batch_size
+
+        # Placeholders
+        self._states = None
+        self._actions = None
+
+        #
+        self._logits = None
+        self._optimizer = None
+        self._var_init = None
+
