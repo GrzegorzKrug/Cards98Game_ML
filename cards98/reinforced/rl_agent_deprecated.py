4b500993e7741dcd5ea3ccde963e489743b65531
Grzegorz Krug <grzesiekk321@gmail.com>
2019-09-08 12:08:56 +0200

    Update rl_agent_deprecated.py

--- cards98/reinforced/rl_agent_deprecated.py
+++ cards98/reinforced/rl_agent_deprecated.py
@@ -20,6 +20,19 @@ class RLAgent:
         self._optimizer = None
         self._var_init = None
 
+        self._define_model()
+
+    def _define_model(self):
+        self._states = tf.compat.v1.placeholder(tf.float32, shape=[None, self._num_states])
+
+        fc1 = tf.layers.dense(self._states, 150, activation=tf.nn.relu)
+
+        # model = Sequential()
+        # fc2 = tf.layers.dense(fc1, 50, activation=tf.nn.relu)
+        # self._logits = tf.layers.dense(fc2, self._num_actions)
+        #
+        # self._qsa = tf.compat.v1.placeholder(tf.float32, shape=[None, self._num_actions])
+
 
 if __name__ == '__main__':
     import sys
@@ -27,5 +40,5 @@ if __name__ == '__main__':
     path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
     sys.path.insert(0, path)
     from game.cards98 import GameCards98
+    agent = RLAgent(num_states=98+4, num_actions=8+4, batch_size=100)
     app = GameCards98()
-    
